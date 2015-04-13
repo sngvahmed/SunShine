6fba64c28596136930696aaafbc349ebb1d124dc
ahmed <ahmednasser1993@gmail.com>
2015-04-14 00:51:40 +0200

    share info with location of me info

--- app/src/main/java/com/sngv/sunshine/Controller/Details/Details_Activity.java
+++ app/src/main/java/com/sngv/sunshine/Controller/Details/Details_Activity.java
@@ -6,7 +6,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import com.sngv.sunshine.Controller.Setting.SettingsActivity;
 import com.sngv.sunshine.R;
 
@@ -31,28 +32,37 @@ public class Details_Activity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_details_, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        ShareActionProvider mShareActionProvider = (ShareActionProvider)MenuItemCompat.getActionProvider(menuItem);
+
+        if(mShareActionProvider != null){
+            mShareActionProvider.setShareIntent(createIntentShare());
+        }
         return true;
+    }
+
+    public String getFromIntentExtra(){
+        return getIntent().getExtras().get(Intent.EXTRA_TEXT).toString();
+    }
+
+    private Intent createIntentShare() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT , getFromIntentExtra());
+        return intent;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+        int id = item.getItemId();
         if (id == R.id.action_settings) {
             Intent intent = new Intent(Details_Activity.this , SettingsActivity.class);
             startActivity(intent);
-
         }
 
         return super.onOptionsItemSelected(item);
     }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-
 }
