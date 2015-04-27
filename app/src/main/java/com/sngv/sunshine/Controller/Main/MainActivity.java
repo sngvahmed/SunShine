131a3ed1090569434fc6784f2b1f04e5b443809f
ahmed <ahmednasser1993@gmail.com>
2015-04-27 19:23:01 +0200

    fix rotate bugs

--- app/src/main/java/com/sngv/sunshine/Controller/Main/MainActivity.java
+++ app/src/main/java/com/sngv/sunshine/Controller/Main/MainActivity.java
@@ -1,6 +1,9 @@
 package com.sngv.sunshine.Controller.Main;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,6 +12,7 @@ import android.widget.Toast;
 
 import com.sngv.sunshine.Controller.Details.DetailActivity;
 import com.sngv.sunshine.Controller.Details.DetailFragment;
+import com.sngv.sunshine.Controller.Setting.SettingsActivity;
 import com.sngv.sunshine.DB.domain.WeatherItem;
 import com.sngv.sunshine.R;
 import com.sngv.sunshine.Service.MultiPanelLisnter;
@@ -16,21 +20,13 @@ import com.sngv.sunshine.Service.MultiPanelLisnter;
 
 public class MainActivity extends ActionBarActivity implements MultiPanelLisnter{
     private boolean mTwoPane = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.main_activity, new MainFragment())
-                    .commit();
-        }
         if (findViewById(R.id.details_activity) != null) {
-//            if (savedInstanceState == null) {
-//                getSupportFragmentManager().beginTransaction()
-//                        .replace(R.id.details_activity, new DetailFragment())
-//                        .commit();
-//            }
+            mTwoPane = true;
         } else {
             mTwoPane = false;
         }
@@ -38,24 +34,39 @@ public class MainActivity extends ActionBarActivity implements MultiPanelLisnter
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this , SettingsActivity.class);
+            startActivity(intent);
             return true;
+        }else if (id == R.id.action_map){
+            openPreferredLocationInMap();
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openPreferredLocationInMap() {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = sharedPref.getString(
+                getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default)
+        );
+
+        Uri uri = Uri.parse(getString(R.string.location_site)).buildUpon().
+                appendQueryParameter("q", location).build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(uri);
+
+        if(intent.resolveActivity(this.getPackageManager()) != null){
+            startActivity(intent);
+        }
     }
 
     @Override
